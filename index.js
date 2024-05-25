01cced6e4f4865d69503214ca3fe3355c7624a0b
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-05-25 11:20:15 +0300

    final commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const input=document.getElementById("input")
+const botton=document.getElementById("button-addon2")
+const todolist =document.getElementById("list")
+document.getElementById("button-addon2").addEventListener("click",function(e){
+    if(input.value===""){
+        alert("kindly write something")
+    }
+    else{
+        const list=document.createElement("list")
+        list.textContent=input.value
+        list.classList.add("list-group-item","category-Tag")
+        todolist.append(list)
+    }
+    input.value=""
+})
+
+list.addEventListener("click", function(e){
+    e.target.remove()
+})
+
+list.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        addItem();
+    }
+});
