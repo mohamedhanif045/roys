728ba1d77021a623564adf97ebaf906936be8808
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-05-26 09:00:04 +0300

    todays coomit add save

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 const input=document.getElementById("input")
-const botton=document.getElementById("button-addon2")
-const todolist =document.getElementById("list")
-document.getElementById("button-addon2").addEventListener("click",function(e){
-    if(input.value===""){
-        alert("kindly write something")
-    }
-    else{
-        const list=document.createElement("list")
-        list.textContent=input.value
-        list.classList.add("list-group-item","category-Tag")
-        todolist.append(list)
+const mm=document.getElementById("list");
+document.getElementById("button-addon2").addEventListener("click",function(){
+    if(input.value=== ""){
+        alert("your input is empty")
+    }else{
+        const list=document.createElement("li");
+        list.textContent=input.value;
+        list.classList.add("list-group-item","category-tag")
+        mm.appendChild(list)
     }
     input.value=""
+    save()
 })
+list.addEventListener("click",function(e){
+// e.target.remove();
+e.target.style.textDecoration="line-through"
+save()
 
-list.addEventListener("click", function(e){
-    e.target.remove()
 })
-
 
 document.addEventListener("keydown",function(e){
-    if((e.key==="Enter")){
-
-        const list=document.createElement("list")
-        list.textContent=input.value;
-        list.classList.add("list-group-item","category-Tag")
-        todolist.append(list)
-        input.value=""
-    }
-   
-    dontRepeat();
+if(e.key === "Enter"){
+    if(input.value === ""){
+        alert("your input is empty")
+        
+    }else{
+    const list=document.createElement("li");
+    list.textContent=input.value;
+    list.classList.add("list-group-item","category-tag")
+    mm.appendChild(list) 
+    input.value=""
+    save()
     
+}
+}
+
 })
+function save(){
+    localStorage.setItem("data", mm.innerHTML)
+}
+function red(){
+    mm.innerHTML=localStorage.getItem("data")
+}
+red()
